10e53fc69c8bf070be62e11b0b9fd0e223d50b55
ajaezokingsley <ajaezokingsley@gmail.com>
2021-12-26 07:13:07 -0800

    completion of the button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Button from './components/elements/button';
+import { useState } from 'react';
+import btnStyle from './components/elements/button/button.module.css';
+
 const App = () => {
-  return <div className="App">Welcome to Algorand </div>;
+  const [loading, setLoading] = useState(true);
+  return (
+    <div className="App">
+      <div style={{ width: '400px' }}>
+        <Button
+          label="Buy Primary"
+          loading={loading}
+          className={[btnStyle.btn_secondary, btnStyle.btn_block].join(' ')}
+          type="button"
+          loadingText="Signing In..."
+        />
+      </div>
+      Welcome to Algorand{' '}
+    </div>
+  );
 };
 
 export default App;
