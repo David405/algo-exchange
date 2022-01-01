0d17ade34d1ced6a67da5a0c79e81936190aef36
ifebrand6 <ifebrand6@gmail.com>
2022-01-01 09:38:45 -0600

    style for branding

--- src/components/layout/containers/mainContent.jsx
+++ src/components/layout/containers/mainContent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import './mainContent.css'
 export  function MainContent(props) {
   const chidlrenComponents = props.children
   return (
     <div className="container-fluid">
-      <div className="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+      <div className="d-sm-flex align-items-center justify-content-between mb-4 bg-white flat-margin">
+        {/* this will dynamically change demanding og */}
+        <h1 className="h3 mb-0 text-gray-800 pl-5">Dashboard</h1>
       </div>
         {chidlrenComponents}
     </div>
--- src/components/widgets/navbar/navbar.jsx
+++ src/components/widgets/navbar/navbar.jsx
@@ -4,7 +4,7 @@ import { DesktopSearchBar } from "../../elements/form";
 import TopBar from "./topbar";
 export function Navbar(params) {
   return (
-    <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+    <nav className="navbar navbar-expand navbar-light  topbar mb-4 static-top ">
       <SidebarMobileToggle />
       <DesktopSearchBar />
       <TopBar />
