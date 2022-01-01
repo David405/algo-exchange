6a4811f0cd750d791784b554c91608a237fa36cb
ifebrand6 <ifebrand6@gmail.com>
2022-01-01 10:02:28 -0600

    finished up with layout

--- src/components/widgets/sidebar/sidebar.jsx
+++ src/components/widgets/sidebar/sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './sidebar.css'
 export default function Sidebar(params) {
   return (
     <>
@@ -7,28 +7,53 @@ export default function Sidebar(params) {
         className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled"
         id="accordionSidebar"
       >
+        {/* TODO change a tag to React Navlink tag */}
         {/* Sidebar - Brand */}
         <a
           className="sidebar-brand d-flex align-items-center justify-content-center"
-          href="index.html"
+          href="#"
         >
           <div className="sidebar-brand-icon rotate-n-15">
-            <i className="fas fa-laugh-wink"></i>
+          <i class="fas fa-funnel-dollar"></i>
           </div>
           {/* INSERT LOGO */}
           <div className="sidebar-brand-text mx-3">
-            SB Admin <sup>2</sup>
+            AlgoExchange
           </div>
         </a>
         {/* Nav Item - Dashboard */}
         <li className="nav-item active">
-          <a className="nav-link" href="index.html">
+          <a className="nav-link" href="#">
             <i className="fas fa-fw fa-tachometer-alt" />
             <span>Dashboard</span>
           </a>
         </li>
+        <li className="nav-item ">
+          <a className="nav-link" href="#">
+            <i class="fas fa-wallet"></i>
+            <span>Wallet</span>
+          </a>
+        </li>
+        <li className="nav-item ">
+          <a className="nav-link" href="#">
+          <i class="fas fa-exchange-alt"></i>
+            <span>Trade</span>
+          </a>
+        </li>
+        <li className="nav-item ">
+          <a className="nav-link" href="#">
+          <i class="fas fa-server"></i>
+            <span>Transactions</span>
+          </a>
+        </li>
+        <li className="nav-item ">
+          <a className="nav-link" href="#">
+          <i class="fas fa-power-off"></i>
+            <span>Logout</span>
+          </a>
+        </li>
         {/* Sidebar Toggler (Sidebar) */}
-        <div className="text-center d-none d-md-inline">
+        <div className="text-center d-none d-md-inline sideBarToggle">
           <button className="rounded-circle border-0" id="sidebarToggle" />
         </div>
       </ul>
