c79961f1955952c2b2db50178bb922baa81a749b
Yibaebi <elliot.yibaebi@yahoo.com>
2022-01-02 20:30:19 +0100

    add input component in home page

--- src/App.js
+++ src/App.js
@@ -1,57 +1,60 @@
-import Button from './components/elements/button';
-import { useState, useEffect, useMemo } from 'react';
-import btnStyle from './components/elements/button/button.module.css';
-import Table from './components/elements/table/table';
-import TableBody from './components/elements/table/tbody';
-import TableHead from './components/elements/table/thead';
-import TableHeader from './components/elements/table/th';
-import TableRow from './components/elements/table/tr';
-import TableData from './components/elements/table/td';
-import axios from 'axios';
-import { useTable } from 'react-table';
+import axios from 'axios'
+import { useEffect, useMemo, useState } from 'react'
+import { useTable } from 'react-table'
+import Button from './components/elements/button'
+import btnStyle from './components/elements/button/button.module.css'
+import { Input } from './components/elements/input'
+import Table from './components/elements/table/table'
+import TableBody from './components/elements/table/tbody'
+import TableData from './components/elements/table/td'
+import TableHeader from './components/elements/table/th'
+import TableHead from './components/elements/table/thead'
+import TableRow from './components/elements/table/tr'
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(true)
+  const [transactions, setTransactions] = useState([])
   const getTransactions = async () => {
     try {
-      const res = await axios.get('https://fakestoreapi.com/products/');
+      const res = await axios.get('https://fakestoreapi.com/products/')
       if (res.status === 200) {
-        console.log(res.data);
-        setTransactions(res.data);
+        console.log(res.data)
+        setTransactions(res.data)
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
-  const _transactionObject =
-    transactions.length > 0 ? Object.keys(transactions[0]) : [];
+  const _transactionObject = transactions.length > 0 ? Object.keys(transactions[0]) : []
   const columns = useMemo(
     () =>
       transactions
         ? _transactionObject
             .filter(key => key !== 'rating')
             .map(key => {
-              return { Header: key, accessor: key };
+              return { Header: key, accessor: key }
             })
         : [],
-    [transactions]
-  );
+    [transactions],
+  )
 
-  const data = useMemo(() => [...transactions], [transactions]);
-  const { getTableProps, getTableBodyProps, rows, headerGroups, prepareRow } =
-    useTable({
-      columns: columns,
-      data: data,
-    });
+  const data = useMemo(() => [...transactions], [transactions])
+  const { getTableProps, getTableBodyProps, rows, headerGroups, prepareRow } = useTable({
+    columns: columns,
+    data: data,
+  })
 
   useEffect(() => {
-    getTransactions();
-  }, []);
+    getTransactions()
+  }, [])
 
   return (
     <div className="App">
+      <div style={{ width: '400px', margin: '30px' }}>
+        <h2 style={{ marginBottom: '20px' }}>Input components</h2>
+        <Input type="text" placeholder="I am an input" />
+      </div>
       <div style={{ width: '400px' }}>
         <Button
           label="Buy Primary"
@@ -68,32 +71,28 @@ const App = () => {
             {headerGroups?.map(headerGroup => (
               <TableRow {...headerGroup.getHeaderGroupProps()}>
                 {headerGroup?.headers.map(column => (
-                  <TableHeader {...column.getHeaderProps()}>
-                    {column.render('Header')}
-                  </TableHeader>
+                  <TableHeader {...column.getHeaderProps()}>{column.render('Header')}</TableHeader>
                 ))}
               </TableRow>
             ))}
           </TableHead>
           <TableBody {...getTableBodyProps()}>
             {rows?.map(row => {
-              prepareRow(row);
+              prepareRow(row)
 
               return (
                 <TableRow {...row.getRowProps()}>
                   {row?.cells.map(cell => (
-                    <TableData {...cell.getCellProps()}>
-                      {cell?.render('Cell')}
-                    </TableData>
+                    <TableData {...cell.getCellProps()}>{cell?.render('Cell')}</TableData>
                   ))}
                 </TableRow>
-              );
+              )
             })}
           </TableBody>
         </Table>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
