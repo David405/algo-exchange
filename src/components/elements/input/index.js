47a829808959a13a4f5efa75acc44a321837be12
Yibaebi <elliot.yibaebi@yahoo.com>
2022-01-02 20:29:07 +0100

    add input component default design

--- src/components/elements/input/index.js
+++ src/components/elements/input/index.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React from 'react'
+import styles from './input.module.css'
 
-const Input = () => {
-  return <div>This is The input Component</div>;
-};
+const Input = ({
+  type = 'text',
+  placeholder = '',
+  handleInputChange = () => {},
+  name = '',
+  value,
+  ref,
+  containerStyles,
+  ...rest
+}) => {
+  return (
+    <div className={styles.input_container} style={containerStyles}>
+      <input
+        className={styles.input}
+        type={type}
+        placeholder={placeholder}
+        onChange={handleInputChange}
+        autoComplete="new-password"
+        name={name}
+        value={value}
+        ref={ref}
+        {...rest}
+      />
+      {rest.error ? <p>{rest.error.message}</p> : null}
+    </div>
+  )
+}
 
-export default Input;
+const TradeInputType = () => {
+  return <div></div>
+}
+
+export { Input }
