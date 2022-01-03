05ed812c261d6761b2e92e72cf3691dabdb5111d
ifebrand6 <ifebrand6@gmail.com>
2022-01-03 07:59:45 -0600

    add default componets for card

--- src/components/elements/card/card.jsx
+++ src/components/elements/card/card.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 export function Card(props) {
   // components ie: trade inputs components
-  // TITLE of the action
-    let {title,components} = props;
+  // TITLE of the action  
+    let {title,components=defaultDiv()} = props;
   return (
     <div className="card shadow mb-4">
       <div className="card-header py-4">
         <h6 className="m-0 text-primary"> {title}</h6>
       </div>
       <div className="card-body">
-        
         {components}
       </div>
     </div>
   );
 }
+const defaultDiv = () => {
+  return (
+    <div className="chart-area"></div>
+  );
+};
