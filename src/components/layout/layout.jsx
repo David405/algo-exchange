3496b508b545d9c5e9243754fad576a9060305f5
Yibaebi <elliot.yibaebi@yahoo.com>
2022-01-04 22:43:09 +0100

    update maincontent container and update layout props

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,28 +1,27 @@
-import React from "react";
-import { Dashboard,Trade,Transactions,Wallet } from "../../pages";
-import "../../vendor/css/style.css";
-import { Navbar } from "../widgets/navbar";
-import Sidebar from "../widgets/sidebar/sidebar";
-import { Wrapper,MainContent } from "./containers/";
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import MainRouter from '../../routes'
+import '../../vendor/css/style.css'
+import { Navbar } from '../widgets/navbar'
+import Sidebar from '../widgets/sidebar/sidebar'
+import { MainContent, Wrapper } from './containers/'
 
 export function Layout(props) {
   return (
-    <>
+    <BrowserRouter>
       <div id="wrapper">
         <Sidebar />
         <Wrapper>
-          <Navbar />
-          <MainContent>
-              {/* pages content */}
-              <Trade title='Trade'/>
+          <MainContent title="Main content">
+            <Navbar />
+            <MainRouter />
           </MainContent>
         </Wrapper>
-
         {/*
         ALERT! Other components/ pages route  will stay inside the main
         ALERT! I am guessing logins and signup will have its on layout 
-        */}
+      */}
       </div>
-    </>
-  );
+    </BrowserRouter>
+  )
 }
