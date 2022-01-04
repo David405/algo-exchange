4289cd7484062b9c13ee7e09186b5681659d0aef
Yibaebi <elliot.yibaebi@yahoo.com>
2022-01-04 22:43:52 +0100

    restructure sidebar links

--- src/components/widgets/sidebar/sidebar.jsx
+++ src/components/widgets/sidebar/sidebar.jsx
@@ -1,62 +1,74 @@
-import React from "react";
-import { logo } from "../../../assets/images";
+import React, { useMemo } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { logo } from '../../../assets/images'
 import './sidebar.css'
 export default function Sidebar(params) {
+  // Loaction object
+  const location = useLocation()
+
+  // Sidebar links
+  const SidebarLinks = useMemo(
+    () => [
+      {
+        linkTitle: 'Dashboard',
+        url: '/',
+        fa_icon: 'fas fa-fw fa-tachometer-alt',
+      },
+      {
+        linkTitle: 'Wallet',
+        url: '/wallet',
+        fa_icon: 'fas fa-wallet',
+      },
+      {
+        linkTitle: 'Trade',
+        url: '/trade',
+        fa_icon: 'fas fa-exchange-alt',
+      },
+      {
+        linkTitle: 'Transactions',
+        url: '/transactions',
+        fa_icon: 'fas fa-server',
+      },
+      {
+        linkTitle: 'Logout',
+        url: '/logout',
+        fa_icon: 'fas fa-power-off',
+      },
+    ],
+    [],
+  )
+
   return (
-    <>
-      <ul
-        className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled"
-        id="accordionSidebar"
-      >
-        {/* TODO change a tag to React Navlink tag */}
-        {/* Sidebar - Brand */}
-        <a
-          className="sidebar-brand d-flex align-items-center justify-content-center"
-          href="#"
-        >
+    <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
+      {/* TODO change a tag to React Navlink tag */}
+      {/* Sidebar - Brand */}
+      <Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/">
+        <span>
           <div className="sidebar-brand-icon rotate-n-15">
-          <i className="fas fa-funnel-dollar"></i>
+            <i className="fas fa-funnel-dollar"></i>
           </div>
           <div className="sidebar-brand-text mx-3">
-            <img src={logo} alt="AlgoExchange"  className="img-fluid"/>
+            <img src={logo} alt="AlgoExchange" className="img-fluid" />
           </div>
-        </a>
-        {/* Nav Item - Dashboard */}
-        <li className="nav-item active">
-          <a className="nav-link" href="#">
-            <i className="fas fa-fw fa-tachometer-alt" />
-            <span>Dashboard</span>
-          </a>
-        </li>
-        <li className="nav-item ">
-          <a className="nav-link" href="#">
-            <i className="fas fa-wallet"></i>
-            <span>Wallet</span>
-          </a>
-        </li>
-        <li className="nav-item ">
-          <a className="nav-link" href="#">
-          <i className="fas fa-exchange-alt"></i>
-            <span>Trade</span>
-          </a>
-        </li>
-        <li className="nav-item ">
-          <a className="nav-link" href="#">
-          <i className="fas fa-server"></i>
-            <span>Transactions</span>
-          </a>
-        </li>
-        <li className="nav-item ">
-          <a className="nav-link" href="#">
-          <i className="fas fa-power-off"></i>
-            <span>Logout</span>
-          </a>
-        </li>
-        {/* Sidebar Toggler (Sidebar) */}
-        <div className="text-center d-none d-md-inline sideBarToggle">
-          <button className="rounded-circle border-0" id="sidebarToggle" />
-        </div>
-      </ul>
-    </>
-  );
+        </span>
+      </Link>
+      {/* Nav Item - Dashboard */}
+      {SidebarLinks.map((linkItem, idx) => {
+        return (
+          <li key={`${linkItem.fa_icon}`} className={`nav-item ${location.pathname === linkItem.url ? 'active' : ''}`}>
+            <Link className="nav-link" to={linkItem.url}>
+              <span>
+                <i className={linkItem.fa_icon} />
+                <span>{linkItem.linkTitle}</span>
+              </span>
+            </Link>
+          </li>
+        )
+      })}
+      {/* Sidebar Toggler (Sidebar) */}
+      <div className="text-center d-none d-md-inline sideBarToggle">
+        <button className="rounded-circle border-0" id="sidebarToggle" />
+      </div>
+    </ul>
+  )
 }
