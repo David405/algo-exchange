cb0115b58695057195d1fe8c13216ad6ead701c1
Yibaebi <elliot.yibaebi@yahoo.com>
2022-01-05 15:46:38 +0100

    minor formatting updates to sidebar component

--- src/components/widgets/sidebar/sidebar.jsx
+++ src/components/widgets/sidebar/sidebar.jsx
@@ -1,48 +1,55 @@
-import React, { useMemo } from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import { logo } from '../../../assets/images'
-import './sidebar.css'
-export default function Sidebar(params) {
-  // Loaction object
-  const location = useLocation()
+import React, { useMemo } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { logo } from "../../../assets/images";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  // Location object
+  const location = useLocation();
 
   // Sidebar links
   const SidebarLinks = useMemo(
     () => [
       {
-        linkTitle: 'Dashboard',
-        url: '/',
-        fa_icon: 'fas fa-fw fa-tachometer-alt',
+        linkTitle: "Dashboard",
+        url: "/",
+        fa_icon: "fas fa-fw fa-tachometer-alt"
       },
       {
-        linkTitle: 'Wallet',
-        url: '/wallet',
-        fa_icon: 'fas fa-wallet',
+        linkTitle: "Wallet",
+        url: "/wallet",
+        fa_icon: "fas fa-wallet"
       },
       {
-        linkTitle: 'Trade',
-        url: '/trade',
-        fa_icon: 'fas fa-exchange-alt',
+        linkTitle: "Trade",
+        url: "/trade",
+        fa_icon: "fas fa-exchange-alt"
       },
       {
-        linkTitle: 'Transactions',
-        url: '/transactions',
-        fa_icon: 'fas fa-server',
+        linkTitle: "Transactions",
+        url: "/transactions",
+        fa_icon: "fas fa-server"
       },
       {
-        linkTitle: 'Logout',
-        url: '/logout',
-        fa_icon: 'fas fa-power-off',
-      },
+        linkTitle: "Logout",
+        url: "/logout",
+        fa_icon: "fas fa-power-off"
+      }
     ],
-    [],
-  )
+    []
+  );
 
   return (
-    <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
+    <ul
+      className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled"
+      id="accordionSidebar"
+    >
       {/* TODO change a tag to React Navlink tag */}
       {/* Sidebar - Brand */}
-      <Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/">
+      <Link
+        className="sidebar-brand d-flex align-items-center justify-content-center"
+        to="/"
+      >
         <span>
           <div className="sidebar-brand-icon rotate-n-15">
             <i className="fas fa-funnel-dollar"></i>
@@ -53,9 +60,14 @@ export default function Sidebar(params) {
         </span>
       </Link>
       {/* Nav Item - Dashboard */}
-      {SidebarLinks.map((linkItem, idx) => {
+      {SidebarLinks.map(linkItem => {
         return (
-          <li key={`${linkItem.fa_icon}`} className={`nav-item ${location.pathname === linkItem.url ? 'active' : ''}`}>
+          <li
+            key={`${linkItem.fa_icon}`}
+            className={`nav-item ${
+              location.pathname === linkItem.url ? "active" : ""
+            }`}
+          >
             <Link className="nav-link" to={linkItem.url}>
               <span>
                 <i className={linkItem.fa_icon} />
@@ -63,12 +75,12 @@ export default function Sidebar(params) {
               </span>
             </Link>
           </li>
-        )
+        );
       })}
       {/* Sidebar Toggler (Sidebar) */}
       <div className="text-center d-none d-md-inline sideBarToggle">
         <button className="rounded-circle border-0" id="sidebarToggle" />
       </div>
     </ul>
-  )
+  );
 }
