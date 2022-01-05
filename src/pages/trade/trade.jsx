e667d8c2a11232e0efb2d66e0a573144cf21093c
Yibaebi <elliot.yibaebi@yahoo.com>
2022-01-05 15:48:28 +0100

    complete trade page coupling

--- src/pages/trade/trade.jsx
+++ src/pages/trade/trade.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Card } from "../../components/elements/card";
-export function Trade(props) {
+import { BuyAndSellWidget } from "../../components/widgets/buyAndSell";
+import { TradeView } from "../../components/widgets/tradeView/index";
+
+export function Trade() {
   return (
     <div className="row">
       <div className="col-xl-7 col-lg-7">
-        <Card title={""} />
-        <Card title={"Trade Center"} />
+        <Card title={"Trade Center"} components={<TradeView />} />
+        <Card title={"Trade Center"} components={<BuyAndSellWidget />} />
       </div>
       <div className="col-xl-5 col-lg-5">
         <Card title={"Order Book"} />
